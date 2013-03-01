bd41f39f429c7ef4a8172e57ef5a41a78e2e03bc
justinjing <jinglijun0612@163.com>
2013-03-01 22:14:51 +0800

    uiwebview用本地资源文件替换server文件的demo

---
+++ Classes/LocalSubstitutionCacheAppDelegate.h
@@ -0,0 +1,28 @@
+//
+//  LocalSubstitutionCacheAppDelegate.h
+//  LocalSubstitutionCache
+//
+//  Created by Matt Gallagher on 2010/09/06.
+//  Copyright Matt Gallagher 2010. All rights reserved.
+//
+//  Permission is given to use this source code file, free of charge, in any
+//  project, commercial or otherwise, entirely at your risk, with the condition
+//  that any redistribution (in part or whole) of source code must retain
+//  this copyright and permission notice. Attribution in compiled projects is
+//  appreciated but not required.
+//
+
+#import <UIKit/UIKit.h>
+
+@class LocalSubstitutionCacheViewController;
+
+@interface LocalSubstitutionCacheAppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+    LocalSubstitutionCacheViewController *viewController;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+@property (nonatomic, retain) IBOutlet LocalSubstitutionCacheViewController *viewController;
+
+@end
+
